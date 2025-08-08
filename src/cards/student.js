382434321d20a4b5b53bb491275f760905497227
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2025-08-08 17:39:32 +0530

    First Commit

---
+++ src/cards/student.js
@@ -0,0 +1,12 @@
+
+const STUDENTS_DATA=[
+{name:"Yugal Shende", email:"yugal@gmail.com", photo:"https://cdn-icons-png.flaticon.com/128/2202/2202112.png",rollnumber:"608794" ,result:"A+"},
+
+ { name:"Rutu Swamy", email:"rutu@gmail.com",photo:"https://cdn-icons-png.flaticon.com/128/921/921124.png", rollnumber:"854534",result:"C-"},
+
+  {name:"Ujwal Lede", email:"uju@gmail.com", photo:"https://cdn-icons-png.flaticon.com/128/219/219970.png" , rollnumber:"980154",result:"B-"},
+
+ { name:"Shreya Katkaye", email:"shreya@gmail.com", photo:"https://cdn-icons-png.flaticon.com/128/11107/11107521.png", rollnumber:"205014",result:"A-"}
+];
+
+export {STUDENTS_DATA}; 
